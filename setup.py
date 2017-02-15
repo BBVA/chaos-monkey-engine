c281e59c970d7c5800bd4b5fe236e9ed3e456a86
Alejandro González <alejandro.gonzalez@intelygenz.com>
2017-02-15 14:20:33 +0100

    Move requirements to setup.py and tox.ini

--- setup.py
+++ setup.py
@@ -17,8 +17,22 @@ setup(name='chaosmonkey',
       url="https://github.com/BBVA/chaos-monkey-engine",
       packages=find_packages(),
       include_package_data=True,
+      install_requires=[
+            'apscheduler==3.3.1',
+            'click==6.7',
+            'apache-libcloud==1.5.0',
+            'paramiko==2.1.1',
+            'requests==2.13.0',
+            'arrow==0.10.0',
+            'flask==0.12',
+            'flask-cors==3.0.2',
+            'flask_sqlalchemy==2.1',
+            'gevent==1.2.1',
+            'SQLAlchemy==1.1.5',
+            'jsonschema==2.6.0',
+            'flask-hal-bbva==1.0.5'
+      ],
       entry_points='''
-        [console_scripts]
-        chaos-monkey-engine=chaosmonkey.cm:cm
-        ''',
-      )
+      [console_scripts]
+      chaos-monkey-engine=chaosmonkey.cm:cm
+      ''')
